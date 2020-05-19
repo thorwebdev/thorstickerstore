1a57234ac277b89c19994ecd26408a76e1a9fadd
Thorsten Schaeff <thorsten@stripe.com>
2020-05-19 16:22:26 +0800

    Store branding & Netlify

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          MJÖLNIR STICKERS COMING SOON...
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://twitter.com/thorwebdev"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ©THORWEBDEV
         </a>
       </header>
     </div>
